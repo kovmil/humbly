5ae80574c56640fac408f9f595c0747c67e9b72f
kovmil <kovmil@gmail.com>
2016-10-09 17:06:27 +0200

    First version of INDEL recognition added

--- humbly.py
+++ humbly.py
@@ -47,7 +47,7 @@ def header():
 
 
 
-    vcf += "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tGENOTYPE\t" + str(sys.argv[1]).replace(".pileup", ".bam")
+    vcf += "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\t" + str(sys.argv[1]).replace(".pileup", ".bam")
 
 #------------------------------- Main program ---------------------------------
 if len(sys.argv) == 1:
@@ -64,34 +64,58 @@ with open(sys.argv[1]) as pileup_file:
 #Variable for printing VCF file
 header()
 
-
 #C-like Typedef struct PiledupStruct
 PileupStruct = namedtuple("PileupStruct", "chrom position ref coverage bases quality")
 
-reg_exp = re.compile(r'(\.*((\+|-)[0-9]+[ATCGN]+)*[ATCGN.]+\.*)+')
-indel_exp = re.compile(r'(\.*[\^$]*((\+|-)[0-9]+[ATCGN]+)*[ATCGN.]+\.*)+')
+reg_exp = re.compile(r'([\.\^\$]*((\+|-)[0-9]+[ATCGN]+)*[ATCGN.]+[\.\^\$]*)+')
 
-
+#(\.*[\^$]*((\+|-)[0-9]+[ATCGN]+)*[ATCGN.]+\.*)+
 
 for iter in lines:
     piled_up = str_to_pileup_struct(iter)
 
     alt = None
     genotype = "0/0"
+    Y_instead_indel = None
 
     #Regex for finding bases which are considered
-    #Ignoring DNA direction (every ',' is '.', 'a' is 'A' etc.)
     match_found = reg_exp.match(str(piled_up.bases))
-    indel_match = indel_exp.match(str(piled_up.bases))
+    #indel_match = indel_exp.match(str(piled_up.bases))
+
     if match_found != None:
 
+        base_len = len(str(match_found.group()))
+
+        if  match_found.group().find('+') >= 0:
+            if(match_found.group()[-1]!='.'):
+                match = str(match_found.group())+"."
+            else:
+                match = str(match_found.group())
+            first_plus_pos = match.find('+')
+            num_of_indels = int(match[first_plus_pos+1])
+            indel = match[first_plus_pos:first_plus_pos+num_of_indels+3]
+            base_len = len(match.replace(indel, "Y"))
+            Y_instead_indel = match.replace(indel, "Y")
+            base_count =  Counter(match.replace(indel, "Y"))
+        elif match_found.group().find('-') >= 0:
+            if(match_found.group()[-1]!='.'):
+                match = str(match_found.group())+"."
+            else:
+                match = str(match_found.group())
+            match = str(match_found.group())
+            first_minus_pos = match.find('-')
+            num_of_indels = int(match[first_minus_pos+1])
+            indel = match[first_minus_pos:first_minus_pos+num_of_indels+3]
+            base_len = len(match.replace(indel, "Y"))
+            Y_instead_indel = match.replace(indel, "Y")
+            base_count =  Counter(match.replace(indel, "Y"))
+        else:
+            base_count = Counter(str(match_found.group()).replace("$","").replace("^",""))
+            Y_instead_indel = str(piled_up.bases)
+
         #Counter is dict subclass for counting the number of occurances of every character in string
-        base_count = Counter(str(match_found.group()))
-        #if base_count['+'] > 0 or base_count['-'] > 0:
-            #print indel_match.group()
 
         mc_list = base_count.most_common(2)
-        base_len = len(str(match_found.group()))
 
         mc = mc_list[0][0]
         mc_cnt = mc_list[0][1]
@@ -121,16 +145,28 @@ for iter in lines:
                     alt = [alt, smc]
                     genotype = "1/2"
 
+
     if genotype != "0/0":
-        q = quality(str(piled_up.bases),str(piled_up.quality), alt)
+        if alt != 'Y':
+            q = quality(Y_instead_indel.replace("$","").replace("^","") ,str(piled_up.quality), alt)
+        else:
+            q = 0
+
+        if alt == 'Y':
+            alt = indel[:-1];
 
         vcf += "\n"+str(piled_up.chrom)
-        vcf += "\t"+str(piled_up.position)
-        vcf += "\t"+str(".")
-        vcf += "\t"+str(piled_up.ref)
-        vcf += "\t"+str(alt)
-        vcf += "\t"+str(q)
-        vcf += "\t"+str(genotype)
+        vcf += "  "+str(piled_up.position)
+        vcf += "  "+str(".")
+        vcf += "  "+str(piled_up.ref)
+        vcf += "  "+str(alt)
+        vcf += "  "+str(q)
+        #Filtering TODO: add filtering function with more parameters
+        if q > 50 and int(piled_up.coverage) >= 5:
+            vcf += "  "+str("PASS")
+        else:
+            vcf += "  "+str(".")
+        vcf += "  "+str(genotype)
 
 print vcf
 #TODO: Add VCF header according to VCF 4.2 standard
