049e6103e59b20d4a5ca1a9d492a62558d042976
kovmil <kovmil@gmail.com>
2016-10-17 23:00:51 +0200

    Known SNP/INDELS VCF support added. MQ, DP and INDEL INFO fields added.

--- humbly.py
+++ humbly.py
@@ -6,11 +6,16 @@
 #------------------------------------------------------------------------------
 import sys
 import re
+import mmap
 from collections import Counter
 from collections import namedtuple
 
 SIGNIFICANT_PERCENTAGE_OF_BASES = (0.25)
 MAJOR_PERCENTAGE_OF_BASES = (0.75)
+
+FILTER_QUALITY = 50
+FILTER_COVERAGE = 5
+KNOWN_CONST = 1.2
 
 vcf = ""
 
@@ -27,6 +32,8 @@ def str_to_pileup_struct(str):
 
 #Function for determining quality of chosen ALT based on quality string provided
 def quality(base, qual, alt):
+    if len(alt) > 1:
+        alt = alt[0]
     positions = [m.start() for m in re.finditer(str(alt), base)]
     num_of_chosen = 1
     if len(positions) > 0:
@@ -42,12 +49,11 @@ def header():
     vcf += "##FILTER=<ID=PASS, Description=\"All filters passed\">\n"
     vcf += "##ALT=<ID=*,Description=\"Represents allele(s) other than observed.\">\n"
     vcf += "##INFO=<ID=INDEL,Number=0,Type=Flag,Description=\"Indicates that the variant is an INDEL.\">\n"
+    vcf += "##INFO=<ID=DP,Number=1,Type=Integer,Description=\"Raw read depth\">\n"
     vcf += "##FORMAT=<ID=PL,Number=G,Type=Integer,Description=\"List of Phred-scaled genotype likelihoods\">\n"
     vcf += "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">\n"
 
-
-
-    vcf += "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\t" + str(sys.argv[1]).replace(".pileup", ".bam")
+    vcf += "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t" + str(sys.argv[1]).replace(".pileup", ".bam")
 
 #------------------------------- Main program ---------------------------------
 if len(sys.argv) == 1:
@@ -67,12 +73,14 @@ header()
 #C-like Typedef struct PiledupStruct
 PileupStruct = namedtuple("PileupStruct", "chrom position ref coverage bases quality")
 
-reg_exp = re.compile(r'([\.\^\$]*((\+|-)[0-9]+[ATCGN]+)*[ATCGN.]+[\.\^\$]*)+')
-
-#(\.*[\^$]*((\+|-)[0-9]+[ATCGN]+)*[ATCGN.]+\.*)+
+reg_exp = re.compile(r'([\.\^\$]*((\+|-)[0-9]+[ATCGN]+)*[ATCGN]+[\.\^\$]*)+')
 
 for iter in lines:
     piled_up = str_to_pileup_struct(iter)
+
+    mq_pos = str(piled_up.bases).find('^')
+    if mq_pos >= 0:
+        mapping_quality = ord(piled_up.bases[mq_pos+1]) - 33
 
     alt = None
     genotype = "0/0"
@@ -80,32 +88,25 @@ for iter in lines:
 
     #Regex for finding bases which are considered
     match_found = reg_exp.match(str(piled_up.bases))
-    #indel_match = indel_exp.match(str(piled_up.bases))
 
     if match_found != None:
-
         base_len = len(str(match_found.group()))
 
-        if  match_found.group().find('+') >= 0:
+        if  match_found.group().find('+') >= 0 or match_found.group().find('-') >= 0:
             if(match_found.group()[-1]!='.'):
                 match = str(match_found.group())+"."
             else:
                 match = str(match_found.group())
-            first_plus_pos = match.find('+')
-            num_of_indels = int(match[first_plus_pos+1])
-            indel = match[first_plus_pos:first_plus_pos+num_of_indels+3]
-            base_len = len(match.replace(indel, "Y"))
-            Y_instead_indel = match.replace(indel, "Y")
-            base_count =  Counter(match.replace(indel, "Y"))
-        elif match_found.group().find('-') >= 0:
-            if(match_found.group()[-1]!='.'):
-                match = str(match_found.group())+"."
-            else:
-                match = str(match_found.group())
-            match = str(match_found.group())
-            first_minus_pos = match.find('-')
-            num_of_indels = int(match[first_minus_pos+1])
-            indel = match[first_minus_pos:first_minus_pos+num_of_indels+3]
+
+            if match.find('+') >= 0:
+                first_plus_pos = match.find('+')
+                num_of_indels = int(match[first_plus_pos+1])
+                indel = match[first_plus_pos:first_plus_pos+num_of_indels+3]
+            elif match.find('-') >= 0:
+                first_minus_pos = match.find('-')
+                num_of_indels = int(match[first_minus_pos+1])
+                indel = match[first_minus_pos:first_minus_pos+num_of_indels+3]
+
             base_len = len(match.replace(indel, "Y"))
             Y_instead_indel = match.replace(indel, "Y")
             base_count =  Counter(match.replace(indel, "Y"))
@@ -113,7 +114,6 @@ for iter in lines:
             base_count = Counter(str(match_found.group()).replace("$","").replace("^",""))
             Y_instead_indel = str(piled_up.bases)
 
-        #Counter is dict subclass for counting the number of occurances of every character in string
 
         mc_list = base_count.most_common(2)
 
@@ -147,29 +147,48 @@ for iter in lines:
 
 
     if genotype != "0/0":
-        if alt != 'Y':
-            q = quality(Y_instead_indel.replace("$","").replace("^","") ,str(piled_up.quality), alt)
-        else:
-            q = 0
+        q = quality(Y_instead_indel.replace("$","").replace("^","") ,str(piled_up.quality), alt)
 
+        known_snp = open('example_human_known_snp.vcf')
+        known_snp_read = mmap.mmap(known_snp.fileno(), 0, access=mmap.ACCESS_READ)
+        if known_snp_read.find("\t"+str(piled_up.position)+"\t") != -1:
+            q = q * KNOWN_CONST
+
+        indel_flag = None
         if alt == 'Y':
-            alt = indel[:-1];
+            indel_flag = "INDEL"
+            alt = indel[:-1]
+            known_indels = open('example_human_known_indels.vcf')
+            known_indel_read = mmap.mmap(known_indels.fileno(), 0, access=mmap.ACCESS_READ)
+            if known_indel_read.find("\t"+str(piled_up.position)+"\t") != -1:
+                q = q * KNOWN_CONST
 
+        #Chrom
         vcf += "\n"+str(piled_up.chrom)
+        #Position
         vcf += "  "+str(piled_up.position)
+        #ID
         vcf += "  "+str(".")
+        #Reference
         vcf += "  "+str(piled_up.ref)
+        #Alt
         vcf += "  "+str(alt)
+        #Quality
         vcf += "  "+str(q)
-        #Filtering TODO: add filtering function with more parameters
-        if q > 50 and int(piled_up.coverage) >= 5:
+        #Filter
+        if q > FILTER_QUALITY and int(piled_up.coverage) >= FILTER_COVERAGE:
             vcf += "  "+str("PASS")
         else:
             vcf += "  "+str(".")
+        #Info
+        vcf += "  "
+        if indel_flag != None:
+            vcf += "INDEL;"
+        vcf += "DP="+str(piled_up.coverage)+";"
+        vcf += "MQ="+str(mapping_quality)+";"
+        #Format
         vcf += "  "+str(genotype)
 
 print vcf
 #TODO: Add VCF header according to VCF 4.2 standard
 #TODO: Add INFO, FORMAT and SAMPLE column and its tags (values) according to 4.2 standard
-#TODO: Add support for taking into account LIST of VCFs with known variants
-#TODO: Quality of the base string and ref. genome will be added in later versions
